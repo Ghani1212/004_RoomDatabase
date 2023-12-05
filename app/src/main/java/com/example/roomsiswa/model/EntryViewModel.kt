2bf403ede9509d26f33a93336bb68924817e8aff
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-12-05 16:58:03 +0700

    add class EntryViewModel

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,2 +1,9 @@
 package com.example.roomsiswa.model
 
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.OfflineRepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa): ViewModel() {
+
+
+}
