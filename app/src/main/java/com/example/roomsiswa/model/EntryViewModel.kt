4530e194920ba056794bdac77248a8ac2dfb1dff
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-12-05 17:06:05 +0700

    Create variabel UIStateSiswa and DetailSiswa in EntryViewModel.kt

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,9 +1,26 @@
 package com.example.roomsiswa.model
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.roomsiswa.repositori.OfflineRepositoriSiswa
 
 class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa): ViewModel() {
+    /*
+           *Berisi status Siswa saat ini
+            */
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
 
-
-}
+    }
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+data class DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
