da39c277c9dc30fdc134766aaff872fe2af50514
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-12-05 17:23:49 +0700

    Create function toDetailSiswa and toUiStateSiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -50,5 +50,16 @@ class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa): ViewM
         alamat = alamat,
         telpon = telpon
     )
+    fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+        detailSiswa = this.toDetailSiswa(),
+        isEntryValid = isEntryValid
+    )
+
+    fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telpon
+    )
 
 }
