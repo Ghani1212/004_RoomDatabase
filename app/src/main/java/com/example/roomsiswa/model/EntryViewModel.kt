afb0d4779d9cca6e2e5bf3b95c30da39cbbca0c3
M S I <ghani.zumar.ft21@mail.umy.ac.id>
2023-12-05 17:13:57 +0700

    add function updateUiState

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -21,6 +21,10 @@ class EntryViewModel(private val repositoriSiswa: OfflineRepositoriSiswa): ViewM
 
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa){
+        uiStateSiswa = UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+
     data class UIStateSiswa(
         val detailSiswa: DetailSiswa = DetailSiswa(),
         val isEntryValid: Boolean = false
